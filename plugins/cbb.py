2123913429fd50ccd3b5e1ecb1bf898451939816
Shashankls <125437933+Shashankls@users.noreply.github.com>
2023-08-12 19:23:07 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -11,7 +11,8 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     if data == "about":
         await query.message.edit_text(
             text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○  
- Channel : @kannada_Rockerssz\n○ Support Group : https://t.me/+l9YAk8zRgldhYWNl</b>",
+ Channel : @kannada_Rockerssz\n○
+ Support Group : https://t.me/+l9YAk8zRgldhYWNl</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
