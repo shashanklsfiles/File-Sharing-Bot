19da177a8f5f2549e23d26c09d00f1ca5ee69a82
Shashankls <125437933+Shashankls@users.noreply.github.com>
2023-08-13 13:25:31 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -3,8 +3,8 @@
   
   
   
- import os 
- import asyncio 
+import os
+import asyncio 
  from pyrogram import Client, filters, __version__ 
  from pyrogram.enums import ParseMode 
  from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
