0f7ba19aa85bf0f0c68bce12e0373c9eec36fc17
shashanklsfiles <152399741+shashanklsfiles@users.noreply.github.com>
2024-06-15 19:17:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5338553454"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://shashanklss:shashank.ls1324@cluster0.cuak2ve.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://shashankls312:shashank.ls1324@cluster0.ib8nobq.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "shash")
 
 #force sub channel id, if you want enable force sub
